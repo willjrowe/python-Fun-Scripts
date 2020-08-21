2b542e8de2b085e5f9d80b3b4443e60fc2b80e8c
willrowe17 <54510998+willrowe17@users.noreply.github.com>
2020-08-21 00:24:37 -0400

    box detection

--- openCVTest.py
+++ openCVTest.py
@@ -1,8 +1,50 @@
 import cv2
 import pytesseract
+import numpy as np
 #why did i install old tesseract
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
-img = cv2.imread("newspaper.jpeg")      
+img = cv2.imread("sudoku.jpeg",0)
+ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+thresh1=255-thresh1
+
+# Defining a kernel length
+kernel_length = np.array(img).shape[1]//80
+ 
+# A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
+verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
+# A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
+hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
+# A kernel of (3 X 3) ones.
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+img_temp1 = cv2.erode(thresh1, verticle_kernel, iterations=3)
+verticle_lines_img = cv2.dilate(img_temp1, verticle_kernel, iterations=3)
+# Morphological operation to detect horizontal lines from an image
+img_temp2 = cv2.erode(thresh1, hori_kernel, iterations=3)
+horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=3)
+alpha = 0.5
+beta = 1.0 - alpha
+img_final_bin = cv2.addWeighted(verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
+img_final_bin = cv2.erode(~img_final_bin, kernel, iterations=2)
+(thresh, img_final_bin) = cv2.threshold(img_final_bin, 128,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+# Find contours for image, which will detect all the boxes
+im2, contours, hierarchy = cv2.findContours(img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# Sort all the contours by top to bottom.
+(contours, boundingBoxes) = sort_contours(contours, method="top-to-bottom")
+idx = 0
+for w in contours:
+    # Returns the location and width,height for every contour
+    x, y, w, h = cv2.boundingRect(c)
+    if (w > 80 and h > 20) and w > 3*h:
+        idx += 1
+        new_img = img[y:y+h, x:x+w]
+        cv2.imwrite(cropped_dir_path+str(idx) + '.png', new_img)
+# If the box height is greater then 20, widht is >80, then only save it as a box in "cropped/" folder.
+    if (w > 80 and h > 20) and w > 3*h:
+        idx += 1
+        new_img = img[y:y+h, x:x+w]
+        cv2.imwrite(cropped_dir_path+str(idx) + '.png', new_img)
+cv2.imshow("img_final_bin.jpg",img_final_bin)
+cv2.waitKey(0)     
 # cv2.namedWindow("output", cv2.WINDOW_NORMAL)                   
 # imgS = cv2.resize(img, (960, 540))                    
 # cv2.imshow("output", imgS)  
@@ -18,8 +60,13 @@ boxes = pytesseract.image_to_boxes(img)
 #     cv2.putText(img,b[0],(x,hImg-y),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
 # cv2.imshow("good",img)
 # cv2.waitKey(0)
-wordBox = pytesseract.image_to_data(img)
+cong = r'--oem 3 --psm 6 outputbase digits'
+wordBox = pytesseract.image_to_data(img,config=cong)
 for x,b in enumerate(wordBox.splitlines()):
     if x!=0:
-        print(b)
+        b=b.split()
+        if len(b) == 12:
+            x,y,w,h = int(b[6]),int(b[7]),int(b[8]),int(b[9])
+            cv2.rectangle(img,(x,y),(w+x,h+y),(0,0,255),3)
+
 print(wordBox)
